8bacc97f023aef84452c5655eb3db4f256c7d898
sungchul-p <sungchul.dev@gmail.com>
2019-07-24 01:44:43 +0900

    [modify] switch 예제 추가

--- src/1_basic/8_switch.go
+++ src/1_basic/8_switch.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -11,14 +13,12 @@ func main() {
 	//자동 break 때문에 fallthrough 존재
 	//Type 분기 -> 값이 아닌 변수 Type 으로 분기 가능
 
-	a := -7
-	switch {
-	case a < 0:
-		fmt.Println(a, "는 음수")
-	case a == 0:
-		fmt.Println(a, "는 0")
-	case a > 0:
-		fmt.Println(a, "는 양수")
+	a := 30 / 15
+	switch a {
+	case 2, 4, 6:
+		fmt.Println("a -> ", a, "짝수")
+	case 1, 3, 5:
+		fmt.Println("a -> ", a, "홀수")
 	}
 
 	switch b := 27; {
@@ -33,10 +33,28 @@ func main() {
 	switch c := "go"; c {
 	case "go":
 		fmt.Println("Go!")
+		fallthrough
 	case "java":
 		fmt.Println("Java!")
+		fallthrough
+	case "python":
+		fmt.Println("Python!")
+		fallthrough
+	case "ruby":
+		fmt.Println("Ruby!")
+		// default:
+		// 	fmt.Println("일치하는 값 없음")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	switch i := rand.Intn(100); {
+	case i >= 50 && i < 100:
+		fmt.Println("i -> ", i, " 50 이상 100미만")
+	case i >= 25 && i < 50:
+		fmt.Println("i -> ", i, " 25 이상 50미만")
 	default:
-		fmt.Println("일치하는 값 없음")
+		fmt.Println("i -> ", i, " 기본 값")
 	}
 
 }
